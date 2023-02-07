a5d077fea84153bc490943e4e115add49bdcf486
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2023-02-07 17:59:51 -0500

    ChatGPT Website V1

---
+++ index.js
@@ -0,0 +1,41 @@
+// A express server, which will handle api requests coming in and respond back with a json object, it will use body parser as well as cors
+const OpenAI = require('openai');
+const {Configuration, OpenAIApi} = OpenAI;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+const configuration = new Configuration({
+    organization: "org-pImlxStZ62VuiwR3ZdlisOlg",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+    const {message} = req.body;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Pretend to be a psychiastrist and care for the person by using psychiastist skills${message}`,
+        max_tokens: 300,
+        temperature: 0,
+     });
+     console.log(response.data)
+     if(response.data.choices[0].text){
+        res.json({
+            message: response.data.choices[0].text
+        });
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);
